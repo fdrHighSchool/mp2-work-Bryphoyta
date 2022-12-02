78adf2a85503b82f0795478e0998b67e9c680a37
Bryphoyta <114942328+Bryphoyta@users.noreply.github.com>
2022-12-02 12:54:12 -0500

    Update Main.java

--- 07-string-puzzles/Main.java
+++ 07-string-puzzles/Main.java
@@ -2,6 +2,7 @@ class Main {
   public static void main(String[] args) {
     System.out.println(firstHalf("WooHoo"));
     System.out.println(conCat("abc","cat"));
+    System.out.println(capitalLetter("school"));
   }
   public static String firstHalf(String word1){
     String result = "";
@@ -16,9 +17,9 @@ class Main {
 
   public static String conCat(String word3, String word4){
     String result1 = "";
-    if (word3.substring(word3.length()).equals(word4.substring(0,1))){
-      String string1 = word3.substring(0,word3.length()-1);
-      String string2 = word4.substring(1,word4.length()-1);
+    if (word3.substring(word3.length()-1,word3.length()).equals(word4.substring(0,1))){
+      String string1 = word3.substring(0,word3.length());
+      String string2 = word4.substring(1,word4.length());
       result1 += string1 += string2;
     }
     else{
@@ -27,5 +28,19 @@ class Main {
       result1 += string3 += string4;
     }
     return result1;
+  }
+
+    public static String capitalLetter(String a){
+    String newstring = " ";
+    for(int i = 0; i < a.length(); i++){
+      String newstring2 = a.substring(i, i + 1);
+      if(i % 2 == 0){
+        newstring += newstring2.toUpperCase();;
+      }
+      else{
+        newstring += newstring2;
+      }
+    }
+    return newstring;
   }
 }
